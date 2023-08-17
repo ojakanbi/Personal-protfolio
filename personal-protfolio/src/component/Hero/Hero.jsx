8378ffefd22eae0505b810637904f3a983e188ed
Oyindamola J Akanbi <ojakanbi@macbook-air.mynetworksettings.com>
2023-08-17 13:05:44 -0400

    Navbar component added to pages

---
+++ personal-protfolio/src/component/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+
+
+export default function Hero () {
+    return (
+        <>
+        <h3>
+            This is the hero page
+        </h3>
+        </>
+    )
+}
