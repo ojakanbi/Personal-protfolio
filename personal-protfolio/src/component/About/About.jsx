a0de7f36aae694224642403a139c939c59260d4c
Oyindamola J Akanbi <ojakanbi@MacBook-Air-3.local>
2023-12-19 12:56:58 -0500

    hero sizing updated & about section updated

--- personal-protfolio/src/component/About/About.jsx
+++ personal-protfolio/src/component/About/About.jsx
@@ -1,63 +1,50 @@
 import React from "react";
-import "./About.css"
-import Image from 'next/image';
-import linkedinPic from "../../images/linkedin-pic.jpg"
-
+import "./About.css";
+import Image from "next/image";
+import linkedinPic from "../../images/linkedin-pic.jpg";
 
 export default function About() {
   return (
     <>
-    <div className="about-container">
-      <div className= "info-container">
-        <div>
-          <div>
-            <h3>About Me</h3>
-            <h6>
-              A Junior majoring in Information Science & Technology at The Pennsylvania State University.
-            </h6>
-            <p>As an<mark> aspiring</mark>tech leader, my passion is centered around harnessing technology to create a positive impact in people's lives. I am driven to build innovative tools and technologies that contribute to the development of society. .</p>
-            <div className="row aabout-list">
-            <div className="media">
-                                        <label>Birthday</label>
-                                        <p>5th april 2003</p>
-                                    </div>
-                                    <div className="media">
-                                        <label>Age</label>
-                                        <p>20 Yr</p>
-                                    </div>
-                                    <div className="media">
-                                        <label>Residence</label>
-                                        <p>Canada</p>
-                                    </div>
-                                    <div className="media">
-                                        <label>Address</label>
-                                        <p>Pennsylvania, USA</p>
-                                    </div>
-                                    <div className="col-md-6">
-                                    <div className="media">
-                                        <label>E-mail</label>
-                                        <p>info@domain.com</p>
-                                    </div>
-                                    <div className="media">
-                                        <label>Phone</label>
-                                        <p>820-885-3321</p>
-                                    </div>
-                                    <div className="media">
-                                        <label>Skype</label>
-                                        <p>skype.0404</p>
-                                    </div>
-                                    <div className="media">
-                                        <label>Freelance</label>
-                                        <p>Available</p>
-                                    </div>
-                                </div>
-
+      <div id="aboutMeTitle">
+        <h3>
+          <span id="who">WHO</span> AM I?{" "}
+        </h3>
+      </div>
+      <div className="hidden about-section" id="about">
+        <div className="about-section-media">
+          {/* <LottieAnimation id="pr" animationData={flag} /> */}
+        </div>
+        <div className="about-section-text">
+          <div className="about-section-text-container">
+            <div className="about-section-text-container-title"></div>
+            <div className="about-section-text-container-content">
+              <p>
+                I am a third-year Nigerian native, currently pursuing a degree
+                in Information Science and Technology. My passion lies in the
+                ever-evolving world of technology and its profound impact on
+                daily human life. As a motivated individual, my goal is to
+                leverage technology to make a positive difference in the world.
+                <span> 
+                  
+           
+            
+                During my leisure time, I find joy in playing soccer with
+                friends and spending quality moments with my family. I dedicated
+                my initial college years to playing soccer at the collegiate
+                level. Additionally, I actively participated in various
+                organizations to enhance my social and leadership skills,
+                including the National Society of Black Engineers (NSBE) and the
+                African Student Association (ASA). Beyond that, I enjoy creating
+                inspirational content on Instagram, aiming to motivate others in
+                areas such as health, fitness, and the college experience.
+                </span>
+              </p>
+              
             </div>
           </div>
-
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
--- personal-protfolio/src/component/Hero/Hero.jsx
+++ personal-protfolio/src/component/Hero/Hero.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import './Hero.css'
-import profile from "../../images/linkedin-pic.jpg"
-import Image from 'next/image';
-
-
-
-
-
+import React from "react";
+import "./Hero.css";
+import profile from "../../images/linkedin-pic.jpg";
+import Image from "next/image";
 
 export default function Hero() {
-    return (
-        <>
-            <div className="mainHeader">
-                <div className="imgContainer">
-                        <Image src={profile} alt="Profile picture of a African American male" id="pf1"/>
-                </div>
-                <div className="textContainer">
-                    <div className="info">
-                        <i className="fa-solid fa-graduation-cap"></i>
-                        <span>The Pennsylvania State University</span>
-                    </div>
-                    <div className="mainName">Oyindamola J Akanbi</div>
-                    <div className="info">
-                        <i className="fa-solid fa-code"></i>
-                        <span>Information Science and Technology</span>
-                    </div>
-                    <a href='pdf/oyindamolaAkanbiResume.pdf' className="resumeLink">
-                        <button className="resumeBtn">RESUME</button>
-                    </a>
-                </div>
-            </div>
-
-
-        </>
-    )
-
+  return (
+    <>
+      <div className="mainHeader">
+        <div className="imgContainer">
+          <Image
+            src={profile}
+            alt="Profile picture of a African American male"
+            id="pf1"
+          />
+        </div>
+        <div className="textContainer">
+          <div className="info">
+            <i className="fa-solid fa-graduation-cap"></i>
+            <span>The Pennsylvania State University</span>
+          </div>
+          <div className="mainName">Oyindamola J Akanbi</div>
+          <div className="info">
+            <i className="fa-solid fa-code"></i>
+            <span>Information Science and Technology</span>
+          </div>
+          <div id="resumeAbout">
+            <a href="pdf/oyindamolaAkanbiResume.pdf" className="resumeLink">
+              <button className="resumeBtn">RESUME</button>
+            </a>
+            <a href="#about" className="resumeLink">
+              <button className="resumeBtn">ABOUT</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
