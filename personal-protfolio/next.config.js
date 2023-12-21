64e2cbb6a24ca895dcfcf4f6798463ae33788a6e
ojakanbiyhl <damola@yellowhouselabs.io>
2023-12-21 00:16:51 -0500

    removed the webpack file

--- personal-protfolio/next.config.js
+++ personal-protfolio/next.config.js
@@ -4,20 +4,3 @@ const nextConfig = {}
 module.exports = nextConfig
 
 
-// module.exports = {
-//     webpack: (config) => {
-//       config.module.rules.push({
-//         test: /\.(pdf)$/,
-//         use: {
-//           loader: 'file-loader',
-//           options: {
-//             name: '[name].[ext]',
-//             publicPath: '/Users/ojakanbi/Desktop/Personal-protfolio/personal-protfolio/public/assests/oyindamolaAkanbiResume.pdf', // Adjust the public path as needed
-//             outputPath: 'pdf/', // Adjust the output path as needed
-//           },
-//         },
-//       });
-//       return config;
-//     },
-//   };
-  
