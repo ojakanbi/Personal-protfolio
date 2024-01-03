128b5dcf5a232777db09c286682879a1465beeab
ojakanbiyhl <damola@yellowhouselabs.io>
2024-01-02 20:25:37 -0500

    target blank on links

--- personal-protfolio/src/component/Projects/Projects.jsx
+++ personal-protfolio/src/component/Projects/Projects.jsx
@@ -11,48 +11,45 @@ import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views-react-18-fix";
 import { autoPlay } from "react-swipeable-views-utils";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import Link from '@mui/material/Link';
-import OpenInBrowserIcon from '@mui/icons-material/OpenInBrowser';
-
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Link from "@mui/material/Link";
+import OpenInBrowserIcon from "@mui/icons-material/OpenInBrowser";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-  
-
 const projects = [
-    {
-        label: "BrainForce",
-        imgPath: "brainforce.png",
-        github: "https://github.com/ojakanbi/BrainForceFrontEnd",
-        website: " https://brainforce-ui.onrender.com/ "
-    },
-    {
-        label: "LifeTracker",
-        imgPath: "lifetracker.png",
-        github: "https://github.com/ojakanbi/LifetrackerSalesforce",
-        website: "https://lifetracker-frontend-oj.onrender.com"
-    },
-    {
-        label: "Flixster",
-        imgPath: "flixster.png",
-        github: "https://github.com/ojakanbi/ojakanbi.github.io",
-        website: "https://ojakanbi.github.io"
-    },
-    {
-      label: "StoreFront",
-      imgPath: "storefront.png",
-      github: "https://github.com/ojakanbi/Slack-store-Salesforce?tab=readme-ov-file",
-      website: "https://ojakanbi.github.io"
+  {
+    label: "BrainForce",
+    imgPath: "brainforce.png",
+    github: "https://github.com/ojakanbi/BrainForceFrontEnd",
+    website: " https://brainforce-ui.onrender.com/ ",
   },
-    {
-        label: "Personal Website",
-        imgPath: "personalweb.png",
-        github: "https://github.com/ojakanbi/Personal-protfolio",
-        website: "https://ojakanbi-ojakanbi.vercel.app/"
-    },
+  {
+    label: "LifeTracker",
+    imgPath: "lifetracker.png",
+    github: "https://github.com/ojakanbi/LifetrackerSalesforce",
+    website: "https://lifetracker-frontend-oj.onrender.com",
+  },
+  {
+    label: "Flixster",
+    imgPath: "flixster.png",
+    github: "https://github.com/ojakanbi/ojakanbi.github.io",
+    website: "https://ojakanbi.github.io",
+  },
+  {
+    label: "StoreFront",
+    imgPath: "storefront.png",
+    github:
+      "https://github.com/ojakanbi/Slack-store-Salesforce?tab=readme-ov-file",
+    website: "https://ojakanbi.github.io",
+  },
+  {
+    label: "Personal Website",
+    imgPath: "personalweb.png",
+    github: "https://github.com/ojakanbi/Personal-protfolio",
+    website: "https://ojakanbi-ojakanbi.vercel.app/",
+  },
 ];
-
 
 function Projects() {
   const theme = useTheme();
@@ -75,14 +72,20 @@ function Projects() {
     <section>
       <div className="section-heading " id="projects">
         <h3>
-                  <span className="span">My</span> Projects{" "}
-                  
-              </h3>
-              
-              
+          <span className="span">My</span> Projects{" "}
+        </h3>
       </div>
       <div className="projects-section">
-        <Box sx={{ maxWidth: 900, flexGrow: 1 , display:"flex", alignContent:"center", flexDirection: "column", justifyContent:"center"}}>
+        <Box
+          sx={{
+            maxWidth: 900,
+            flexGrow: 1,
+            display: "flex",
+            alignContent: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
           <Paper
             square
             elevation={0}
@@ -99,15 +102,16 @@ function Projects() {
               padding: "5px",
             }}
           >
-                      <Typography
-                          
-                          sx={{
-                              color: "whitee",
-                              fontWeight: "bold",
-                              fontSize: "30px",
-                                textAlign: "center",
-                          }}
-                      >{projects[activeStep].label}</Typography>
+            <Typography
+              sx={{
+                color: "whitee",
+                fontWeight: "bold",
+                fontSize: "30px",
+                textAlign: "center",
+              }}
+            >
+              {projects[activeStep].label}
+            </Typography>
 
             <AutoPlaySwipeableViews
               axis={theme.direction === "rtl" ? "x-reverse" : "x"}
@@ -125,59 +129,54 @@ function Projects() {
                         display: "block",
                         maxWidth: 800,
                         objectFit: "cover",
-                          margin: "auto",
+                        margin: "auto",
                         marginTop: "10px",
                         borderRadius: "15px",
                       }}
                       src={step.imgPath}
                       alt={step.label}
-                          />
-                          
-                          
-                      ) : null}
-                      
+                    />
+                  ) : null}
                 </div>
               ))}
-                      </AutoPlaySwipeableViews>
-                      
-                      <Typography
-                          
-                          sx={{
-                              color: "whitee",
-                              fontWeight: "bold",
-                              fontSize: "30px",
-                                textAlign: "center",
-                          }}
-                      >
-                          <Link
-                                sx={{
-                                    color: "white",
-                                    fontWeight: "bold",
-                                    fontSize: "30px",
-                                  textAlign: "center",
-                                    marginRight: "10px",
+            </AutoPlaySwipeableViews>
 
-                                }}
-                          
-                              href={projects[activeStep].github}><GitHubIcon></GitHubIcon></Link>
-                        
-                          <Link
-                                sx={{
-                                    color: "white",
-                                    fontWeight: "bold",
-                                    fontSize: "30px",
-                                  textAlign: "center",
+            <Typography
+              sx={{
+                color: "whitee",
+                fontWeight: "bold",
+                fontSize: "30px",
+                textAlign: "center",
+              }}
+            >
+              <Link
+                target="_blank"
+                sx={{
+                  color: "white",
+                  fontWeight: "bold",
+                  fontSize: "30px",
+                  textAlign: "center",
+                  marginRight: "10px",
+                }}
+                href={projects[activeStep].github}
+              >
+                <GitHubIcon></GitHubIcon>
+              </Link>
 
-
-                                }}
-                          
-                              href={projects[activeStep].website}><OpenInBrowserIcon></OpenInBrowserIcon></Link>
-                          
-                    
-                           </Typography>
-                  </Paper>
-                  
-                  
+              <Link
+                target="_blank"
+                sx={{
+                  color: "white",
+                  fontWeight: "bold",
+                  fontSize: "30px",
+                  textAlign: "center",
+                }}
+                href={projects[activeStep].website}
+              >
+                <OpenInBrowserIcon></OpenInBrowserIcon>
+              </Link>
+            </Typography>
+          </Paper>
 
           <MobileStepper
             sx={{
@@ -188,7 +187,7 @@ function Projects() {
               fontWeight: "bold",
               marginBottom: "100px",
               background: "#444",
-              width: "100%"
+              width: "100%",
             }}
             steps={maxSteps}
             position="static"
